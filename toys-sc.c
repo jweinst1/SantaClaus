81f909534c137fe91f48ba36e55746fe49c500d2
Joshua Weinstein <jweinstein@splunk.com>
2020-11-02 15:52:25 -0800

    desc builder

--- toys-sc.c
+++ toys-sc.c
@@ -1,3 +1,34 @@
 #include "toys-sc.h"
+#include <assert.h>
 
-static int test = 1;
+#ifndef SANTA_CLAUS_TOY_BUILDER_CAP
+#define SANTA_CLAUS_TOY_BUILDER_CAP 10
+#endif
+
+void ScToyDescSeqBuilder_start(ScToyDescSeqBuilder* b)
+{
+    b->len = 0;
+    b->cap = SANTA_CLAUS_TOY_BUILDER_CAP;
+    b->descs = malloc(SANTA_CLAUS_TOY_BUILDER_CAP);
+    assert(b->descs);
+}
+
+void ScToyDescSeqBuilder_add(ScToyDescSeqBuilder* b, ScToyDesc desc)
+{
+    if (b->len == b->cap) {
+        b->cap *= 3;
+        b->descs = realloc(b->descs, b->cap);
+        assert(b->descs);
+    }
+    b->descs[b->len++] = desc;
+}
+
+void ScToyDescSeqBuilder_finish(ScToyDescSeqBuilder* b, ScToyDescSeq* s)
+{
+    s->size = b->len;
+    s->descs = b->descs;
+    b->cap = 0;
+    b->len = 0;
+    b->descs = NULL;
+}
+
--- toys-sc.h
+++ toys-sc.h
@@ -6,6 +6,41 @@
 
 typedef enum {
    TOY_DESC_TYPE_RED
-} toy_desc_t;
+} ScToyDesc;
+
+typedef struct {
+    size_t size;
+    ScToyDesc* descs;
+} ScToyDescSeq;
+
+typedef struct {
+   size_t len;
+   size_t cap;
+   ScToyDesc* descs;
+} ScToyDescSeqBuilder;
+
+typedef enum {
+    SC_TOY_OBJECT_HORSE
+} ScToyObj;
+
+typedef struct {
+    ScToyDescSeq descs;
+    ScToyObj obj;
+} ScToy;
+
+/**
+ * This starts a descriptor sequence, and allows new descs to be
+ * appended to it.
+ */
+extern void ScToyDescSeqBuilder_start(ScToyDescSeqBuilder* b);
+/**
+ * This appends a desc and reallocs as needed.
+ */
+extern void ScToyDescSeqBuilder_add(ScToyDescSeqBuilder* b, ScToyDesc desc);
+/**
+ * This transfers the inner desc pointer to a seq obj.
+ * Warning, this transfers ownership of memory
+ */
+extern void ScToyDescSeqBuilder_finish(ScToyDescSeqBuilder* b, ScToyDescSeq* s);
 
 #endif // TOYS_SANTA_CLAUS_H
